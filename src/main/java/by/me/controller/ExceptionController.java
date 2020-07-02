f7d689b32dfb2f85803ef2009e6c7f7fef1ed21b
IseTheSal <illya.ageevich@gmail.com>
2020-07-02 21:10:18 +0300

    Fixed ExceptionController

--- src/main/java/by/me/controller/ExceptionController.java
+++ src/main/java/by/me/controller/ExceptionController.java
@@ -2,6 +2,7 @@ package by.me.controller;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import by.me.service.UserNotFoundException;
 import org.springframework.ui.Model;
 
 
@@ -10,6 +11,13 @@ public class ExceptionController {
 
     @ExceptionHandler(NullPointerException.class)
     public String ex(NullPointerException ex, Model model) {
+        String message = ex.getMessage();
+        model.addAttribute("message", message);
+        return "userauth";
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public String ex (UserNotFoundException ex, Model model){
         String message = ex.getMessage();
         model.addAttribute("message", message);
         return "userauth";
